6465ce6da6f57632e6a1b72c9dadc1200d99389c
Vivek Phull <vivek.phull@student.adelaide.edu.au>
2017-09-30 09:42:36 +0930

    almost done

--- app/src/main/java/com/pavi_developing/myaustralialogin/MappedReports.java
+++ app/src/main/java/com/pavi_developing/myaustralialogin/MappedReports.java
@@ -1,13 +1,23 @@
 package com.pavi_developing.myaustralialogin;
 
+import android.*;
+import android.Manifest;
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -19,12 +29,37 @@ import com.google.android.gms.maps.model.MarkerOptions;
  * Created by VivekPhull on 9/29/2017.
  */
 
-public class MappedReports extends FragmentActivity implements OnMapReadyCallback {
+public class MappedReports extends FragmentActivity
+        implements OnMapReadyCallback,
+        LocationListener {
+
+    GoogleMap mMap;
+    private LocationManager locationManager;
+    private static final long MIN_TIME = 400;
+    private static final float MIN_DISTANCE = 1000;
+    private boolean locationPermission;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         initializeViews(getApplicationContext());
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationPermission = true;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PackageManager.PERMISSION_GRANTED);
+            }
+        }
+
     }
 
     private void initializeViews(Context context) {
@@ -35,7 +70,7 @@ public class MappedReports extends FragmentActivity implements OnMapReadyCallbac
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
+        mMap = googleMap;
     }
 
     private MarkerOptions getMarker(LatLng latLng, String title, int resource) {
@@ -48,4 +83,19 @@ public class MappedReports extends FragmentActivity implements OnMapReadyCallbac
         return mark;
     }
 
+    private void moveCamera(Location location) {
+        LatLng latLong = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLong));
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME, MIN_DISTANCE, (android.location.LocationListener) this); //You can also use LocationManager.GPS_PROVIDER and LocationManager.PASSIVE_PROVIDER
+    }
 }
